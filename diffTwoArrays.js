ea90a47f6b7dba4dabb66c661b8f81c216600bef
Joshua Ruff <39273377+joshuaRuff@users.noreply.github.com>
2018-09-18 22:30:10 -0500

    Update diffTwoArrays.js

--- diffTwoArrays.js
+++ diffTwoArrays.js
@@ -2,14 +2,14 @@ function diffArray(arr1, arr2) {
   const newArr = [];
   let longerArr, shorterArr;
 
-    //Compare the length of the arrays and set them as new variables
-    if(arr1.length >= arr2.length) {
-      longerArr = arr1;
-      shorterArr = arr2;
-    } else {
-      longerArr = arr2;
-      shorterArr = arr1
-    }
+  //Compare the length of the arrays and set them as new variables
+  if(arr1.length >= arr2.length) {
+    longerArr = arr1;
+    shorterArr = arr2;
+  } else {
+    longerArr = arr2;
+    shorterArr = arr1
+  }
 
   //Use indexOf to test the longerArr against the shorterArr pushing differences to newArr
   for(let i = 0; i < longerArr.length; i++) {
@@ -20,7 +20,7 @@ function diffArray(arr1, arr2) {
 
   //Use indexOf to test the shorterArr against the longerArr pushing differences to newArr
   for(let i = 0; i < shorterArr.length; i++) {
-      if(longerArr.indexOf(shorterArr[i]) === -1) {
+    if(longerArr.indexOf(shorterArr[i]) === -1) {
       newArr.push(shorterArr[i]);
     }
   } 
