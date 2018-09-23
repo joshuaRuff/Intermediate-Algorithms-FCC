f1f456749ac377420214929b3e3e9c563a4d0623
Joshua Ruff <39273377+joshuaRuff@users.noreply.github.com>
2018-09-23 17:26:15 -0500

    Update seekAndDestroy.js

--- seekAndDestroy.js
+++ seekAndDestroy.js
@@ -1,13 +1,12 @@
 //Create rest array with any number of additional params
 function destroyer(arr, ...num) {
 //create a holderArray to return
-  let holderArray = []
-  
+  let holderArray = [];
 //Loop through initial array and test if each element can be found in the rest array
   for(let i = 0; i < arr.length; i++) {
 //If element can't be found add this element to the holderArray
     if(num.indexOf(arr[i]) === -1) {
-      holderArray.push(arr[i])
+      holderArray.push(arr[i]);
     } 
   }
 //Return holderArray
